93b476c53acb07831b0513873b08394ee93e46d4
NONTorials <81377915+NONTorials@users.noreply.github.com>
2021-04-01 19:55:11 -0600

    Inicios
    Forceban Bot Base

---
+++ index.js
@@ -0,0 +1,2 @@
+//Este serán 2 comandos para la forceban, no usaré handler porque es algo relativamente rapido y facil y para los gilis, usaré megadb y no mongodb porque se confunden
+const Discord = require('discord.js'); //Declaramos discord
