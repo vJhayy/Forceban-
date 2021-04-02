d7f0da985717a26c783a528aa8d9389eb63e2268
NONTorials <81377915+NONTorials@users.noreply.github.com>
2021-04-02 11:09:14 -0600

    Forceadd
    No Copy and Paste

--- index.js
+++ index.js
@@ -11,8 +11,33 @@ client.on('ready', async() =>{
 console.log(client.user.tag+' forceban lista!');
 });
 client.on('message' , async message =>{
-
+    //Si el mensaje es de un MD o el autor es un bot o el mensaje no empieza con el prefix, retorna
+if(message.channel.type === "dm" || message.author.bot || !message.content.startsWith(prefix)) return;
+    
+    
+    //Args
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
-
+//Commmand
+    let cmd = args.shift().toLowerCase();
+    
+    //DB
+    const forceban = new client.db('Forceban');
+    
+    
+    //Cmds
+    if(cmd === "force"){
+        //Si la ID del autor del mensaje no esta en el array de la config de developers del bot, retorna
+        if(!client.config.developers.includes(message.author.id); return;
+        let metodo = args[0];
+        if(!metodo) return; //Si no introduce el metodo, retorna
+        metodo = args[0].toLowerCase(); 
+        
+        
+        if(metodo === "add"){
+        //si escribe como metodo add  
+            
+            
+        }
+    }
   
 });
