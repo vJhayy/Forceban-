6277392873f0d8b6d55a09fbb60186e3745e26ef
NONTorials <81377915+NONTorials@users.noreply.github.com>
2021-04-01 19:59:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 //Este serán 2 comandos para la forceban, no usaré handler porque es algo relativamente rapido y facil y para los gilis, usaré megadb y no mongodb porque se confunden
 const Discord = require('discord.js'); //Declaramos discord
+const client = new Discord.Client(); //Usamos el constructor del cliente
+//Client
+client.config = require('./config.json'); //Config
+client.db = require('megabd'); //Db del bot
+//Eventos 
+client.on('ready', async() =>{
+console.log(client.user.tag+' forceban lista!');
+});
+client.on('message' , async message =>{
+
+  
+  
+});
